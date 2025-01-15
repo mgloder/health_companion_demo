2e9cf6659ccbcb3c1b2e0221c4b5dfbf3f71051e
Jiaxin Luo <luojiaxin.studio@gmail.com>
2025-01-15 14:56:42 +0800

    feat: add exercise panel for home page

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,4 +1,43 @@
 import { Link } from "react-router-dom";
+import { Calendar, ChevronRight } from 'react-feather';
+
+function Switch() {
+  return (
+    <div className="relative flex p-4 justify-center gap-4">
+      <div className="flex flex-1 justify-center items-center rounded-2xl shadow-lg h-16 bg-black text-white">
+        <p className="text-4xl">315.7<span className="text-base font-light ml-2">hours</span></p>
+      </div>
+      <div className="absolute left-1/2 top-1/2 z-20 m-auto flex -translate-x-1/2 -translate-y-1/2 transform">
+        <div
+          className="inline-flex rounded-full border border-solid border-gray-250 bg-white p-1">
+          <div
+            className="inline-flex rounded-full border border-solid bg-lime-300 p-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 17" aria-hidden="true"
+                 className="h-4 w-4 text-black">
+              <path fill="currentColor" fill-rule="evenodd"
+                    d="M11.726 1.273l2.387 2.394H.667V5h13.446l-2.386 2.393.94.94 4-4-4-4-.94.94zM.666 12.333l4 4 .94-.94L3.22 13h13.447v-1.333H3.22l2.386-2.394-.94-.94-4 4z"
+                    clip-rule="evenodd"></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-1 justify-center items-center rounded-2xl shadow-lg h-16 bg-black text-white">
+        <p className="text-4xl">69.00<span className="text-base font-light ml-2">mbc</span></p>
+      </div>
+    </div>
+  );
+}
+
+function ExercisePanel({ className}) {
+  return (
+    <div className={className}>
+      <div className="flex justify-between items-center">
+        <p className="text-sky-600">NO.<span className="text-xl">5</span><span className="text-gray-400 pl-2">Among Friends</span><ChevronRight className="inline-block h-4 w-4 pl-1"/></p>
+        <Calendar className="inline-block h-4 w-4" />
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
@@ -11,25 +50,8 @@ export default function Home() {
                alt="" />
         </Link>
       </div>
-      <div className="relative flex p-4 justify-center gap-4">
-        <div className="flex flex-1 justify-center items-center rounded-2xl h-16 bg-black text-white">
-          <p className="text-4xl">315.7<span className="text-base font-light ml-2">hours</span></p>
-        </div>
-        <div className="absolute left-1/2 top-1/2 z-20 m-auto flex -translate-x-1/2 -translate-y-1/2 transform">
-          <button
-            className="inline-flex rounded-full border border-solid border-gray-250 bg-white p-4 hover:bg-gray-150">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 17" aria-hidden="true"
-                 className="h-4 w-4 rotate-90 text-greyblue-400 md:rotate-0">
-              <path fill="currentColor" fill-rule="evenodd"
-                    d="M11.726 1.273l2.387 2.394H.667V5h13.446l-2.386 2.393.94.94 4-4-4-4-.94.94zM.666 12.333l4 4 .94-.94L3.22 13h13.447v-1.333H3.22l2.386-2.394-.94-.94-4 4z"
-                    clip-rule="evenodd"></path>
-            </svg>
-          </button>
-        </div>
-        <div className="flex flex-1 justify-center items-center rounded-2xl h-16 bg-black text-white">
-          <p className="text-4xl">69.00<span className="text-base font-light ml-2">mbc</span></p>
-        </div>
-      </div>
+      <Switch />
+      <ExercisePanel className="mt-1 px-12"/>
     </>
   );
 }
