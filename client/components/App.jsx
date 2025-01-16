75def0b9cd41e63bc0b93ffce96c7f053454b482
Jiaxin Luo <luojiaxin.studio@gmail.com>
2025-01-16 09:34:56 +0800

    refactor: move home page to index page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 // import logo from "/assets/openai-logomark.svg";
 import EventLog from "./EventLog";
 import SessionControls from "./SessionControls";
@@ -130,12 +130,6 @@ export default function App() {
         <div className="flex items-center gap-4 w-full m-4 pb-2 border-0 border-b border-solid border-gray-200">
           {/* <img style={{ width: "24px" }} src={logo} /> */}
           <h1>realtime console</h1>
-          <p>
-            <Link to="/home">Go to home page</Link>
-          </p>
-          <p>
-            <Link to="/chat">Go to Chat page</Link>
-          </p>
         </div>
       </nav>
       <main className="absolute top-16 left-0 right-0 bottom-0">
