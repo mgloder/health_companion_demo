0d04002a8883756c547d2cc8c8b735a78a6da31c
Jiaxin Luo <luojiaxin.studio@gmail.com>
2025-01-16 10:42:14 +0800

    feat: chat page static ui

--- client/pages/chat.jsx
+++ client/pages/chat.jsx
@@ -1,12 +1,20 @@
-import { Link } from 'react-router-dom'
+import { PhoneCall } from "react-feather";
+import endCall from "../assets/end-call.svg";
 
 export default function Chat() {
   return (
-    <>
-      <h1 className="text-6xl">Chat</h1>
-      <p>
-        <Link to="/">Go back to the AI Health</Link>
-      </p>
-    </>
+    <div className="h-screen relative bg-gradient-to-b from-blue-400 to-violet-200">
+      <div className="text-white text-4xl absolute left-[15%] top-1/4">
+        <h1>Hello Marry,</h1>
+        <h1>Let's check in.</h1>
+      </div>
+      <div className="absolute left-1/2 top-[70%] flex items-center -translate-x-1/2 -translate-y-1/2">
+        <PhoneCall className="inline-block -ml-3" />
+        <span className="ml-2">00:01:35</span>
+      </div>
+      <div className="absolute left-1/2 top-3/4 -translate-x-1/2">
+        <img className="w-16" src={endCall} />
+      </div>
+    </div>
   )
 }
